639fbfa1251092b9882a0b735d77bee7f71d6a43
Anand P <anandpamegoze@gmail.com>
2024-03-06 11:30:54 +0530

    add filter bank and other filters

---
+++ gen.py
@@ -0,0 +1,116 @@
+import numpy as np
+import skimage
+from skimage import img_as_float
+import cv2
+import matplotlib.pyplot as plt
+import random
+import time
+from skimage.metrics import structural_similarity as ssim_metric
+
+filter_size = 3
+population_size = 50
+num_generations = 500
+threshold_value = 34
+
+# image to float
+
+
+def add_speckle_noise_test(image, sigma):
+    # Set a random seed based on the current time
+    np.random.seed(int(time.time()))
+
+    # Generate a random Gaussian noise array with the same shape as the image.
+    noise = np.random.normal(0, sigma, image.shape)
+
+    # Multiply the noise array by the image to add speckle noise to the image.
+    noisy_image = image + image * noise
+
+    return noisy_image
+
+
+def create_random_filter(size):
+    matrix = np.random.rand(size, size)
+    matrix /= matrix.sum()
+    return matrix
+
+
+def apply_filter(image, filter):
+    return img_as_float(cv2.filter2D(image, -1, filter))
+
+
+def apply_filter2(image, filter):
+    return img_as_float(cv2.filter2D(image, -1, filter))
+
+
+def calculate_psnr(image1, image2):
+    mse = np.mean((image1 - image2) ** 2)
+    if mse == 0:
+        return 100
+    max_pixel = 1
+    psnr = 20 * np.log10(max_pixel / np.sqrt(mse))
+    return psnr
+
+
+def fitness(original_image, noisy_image, filter):
+    filtered_image = apply_filter(noisy_image, filter)
+    psnr = calculate_psnr(original_image, filtered_image)
+    return psnr
+
+
+def calculate_ssim(img1, img2):
+    return ssim_metric(img1, img2)
+
+
+def genetic_algorithm(
+    original_image, noisy_image, filter_size, population_size, num_generations
+):
+    population = [create_random_filter(filter_size) for _ in range(population_size)]
+    for generation in range(num_generations):
+        print(generation)
+        fitness_scores = [
+            fitness(original_image, noisy_image, filter) for filter in population
+        ]
+        selected_indices = np.argsort(fitness_scores)[
+            -1 : (population_size // 2) - 1 : -1
+        ]
+        parents = [population[i] for i in selected_indices]
+        min_fitness_value = min(fitness_scores)
+        if fitness_scores[selected_indices[0]] >= 32:
+            print("found")
+            return parents[0]
+        offspring = parents
+        while len(offspring) < population_size:
+            p1 = random.choice(selected_indices)
+            p2 = random.choice(selected_indices)
+            parent1 = population[p1]
+            parent2 = population[p2]
+            child = crossover(parent1, parent2)
+            child = mutate(child)
+            fitness_child = fitness(original_image, noisy_image, child)
+            if fitness_child >= min_fitness_value:
+                offspring.append(child)
+        population = offspring
+    best_filter = population[np.argmax(fitness_scores)]
+    return best_filter
+
+
+def crossover(parent1, parent2):
+    crossover_point = np.random.randint(0, filter_size * filter_size)
+    flattened_parent1 = parent1.reshape(-1)
+    flattened_parent2 = parent2.reshape(-1)
+    child1 = np.concatenate(
+        (flattened_parent1[:crossover_point], flattened_parent2[crossover_point:])
+    )
+    child1 = child1.reshape(filter_size, filter_size)
+    return child1
+
+
+def mutate(filter):
+
+    flattened_filter = filter.flatten()
+    l = len(flattened_filter)
+    limit1 = random.randint(0, l)
+    limit2 = random.randint(limit1, l)
+    np.random.shuffle(flattened_filter[limit1:limit2])
+    mutated_filter = flattened_filter.reshape(filter.shape)
+    return mutated_filter
---
+++ otherfilters.py
@@ -0,0 +1,33 @@
+from scipy.ndimage import uniform_filter
+import numpy as np
+import bm3d
+
+# lee filter
+def lee_filter(img, kernel_size):
+    img_mean = uniform_filter(img, (kernel_size, kernel_size))
+    img_sqr_mean = uniform_filter(img**2, (kernel_size, kernel_size))
+    img_variance = img_sqr_mean - img_mean**2
+    overall_variance = np.var(img)
+    img_weights = img_variance / (img_variance + overall_variance)
+    img_output = img_mean + img_weights * (img - img_mean)
+    return img_output
+    
+
+def frost_filter(img, kernel_size, damping_factor=2.0):
+    h, w = img.shape
+    padded_img = np.pad(img, kernel_size // 2, mode="reflect")
+    filtered_img = np.zeros_like(img, dtype=np.float64)
+    for i in range(h):
+        for j in range(w):
+            window = padded_img[i : i + kernel_size, j : j + kernel_size]
+            center_pixel = window[kernel_size // 2, kernel_size // 2]
+            distances = (window - center_pixel) ** 2
+            weights = np.exp(-damping_factor * distances)
+            filtered_img[i, j] = np.sum(window * weights) / np.sum(weights)
+    return filtered_img
+    
+def bm3d_filter(image_noisy, sigma_psd=0.1):
+    image_noisy_float = np.float32(image_noisy)
+    denoised_image = bm3d.bm3d(image_noisy_float, sigma_psd=sigma_psd)
+    denoised_image = np.uint8(denoised_image)
+    return denoised_image
